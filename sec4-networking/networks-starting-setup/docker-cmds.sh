6a6d1a34007612d665db57517f4b41784ff4d483
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2021-10-05 21:58:56 +0530

    Code changes for Section 4

---
+++ sec4-networking/networks-starting-setup/docker-cmds.sh
@@ -0,0 +1,19 @@
+# How to install postman for api testing
+https://www.tecmint.com/install-postman-on-linux-desktop/
+
+# find container ip address
+docker container inspect mongodb
+
+# build app image
+docker build -t favorites .
+
+# create internal network
+docker network create favorites
+docker network ls
+
+# create container for mongodb inside above created internal network
+docker run -d --name mongodb --network favorites mongo
+
+# create container for app
+docker run -d --name favorites -p 3000:3000 -v "$(pwd):/app" -v /app/node_modules --network favorites favorites
+
